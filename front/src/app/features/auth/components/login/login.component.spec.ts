9de7fc48fcbae7fa1e36c57bc45f78fa5209f828
Yoann DREVET <yoann.drevet0@laposte.net>
2024-07-31 01:25:11 +0200

    Unit tests for Login form

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -11,6 +11,7 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import { By } from "@angular/platform-browser";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -38,5 +39,56 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display an error message on error condition', () => {
+    component.onError = true;
+    fixture.detectChanges();
+    const errorElement = fixture.debugElement.query(By.css('.error'));
+
+    // Expect element to exist
+    expect(errorElement).toBeTruthy();
+    // ...and to display the proper error message
+    expect(errorElement.nativeElement.innerHTML).toEqual('An error occurred');
+  });
+
+  it('shold not be valid if missing email', () => {
+    component.form.setValue({
+      email: '',
+      password: 'password',
+    });
+    expect(component.form.valid).toBe(false);
+  });
+
+  it('should not be valid if missing password', () => {
+    component.form.setValue({
+      email: 'test@test.com',
+      password: '',
+    });
+    expect(component.form.valid).toBe(false);
+  });
+
+  it('should not be valid if email is malformed', () => {
+    component.form.setValue({
+      email: 'thisisnotavalidemailaddress',
+      password: 'goodP@ssw0rd',
+    });
+    expect(component.form.valid).toBe(false);
+  });
+
+  it('should not be valid if password is under 3 characters', () => {
+    component.form.setValue({
+      email: 'test@test.com',
+      password: '12',
+    });
+    expect(component.form.valid).toBe(false);
+  });
+
+  it('should be valid if both fields provided and valid', () => {
+    component.form.setValue({
+      email: 'test@test.com',
+      password: 'goodP@ssw0rd',
+    });
+    expect(component.form.valid).toBe(true);
   });
 });
