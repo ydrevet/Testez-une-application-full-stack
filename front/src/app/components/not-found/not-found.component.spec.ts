080f6c6ffcebe90b488f5505f1503442f05d23c1
Yoann DREVET <yoann.drevet0@laposte.net>
2024-07-31 04:58:57 +0200

    Useless quick win

--- front/src/app/components/not-found/not-found.component.spec.ts
+++ front/src/app/components/not-found/not-found.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { NotFoundComponent } from './not-found.component';
+import {By} from "@angular/platform-browser";
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
@@ -20,5 +21,10 @@ describe('NotFoundComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show a page not found message', () => {
+    const headingElement = fixture.debugElement.query(By.css('h1'));
+    expect(headingElement.nativeElement.innerHTML).toEqual('Page not found !');
   });
 });
